37989d90da1820d26516de5484138eef7828f6c6
leo4211 <liuyangaijizhilei@126.com>
2016-08-30 15:23:28 +0800

    新建用户实体类

---
+++ app/src/main/java/test/liuyang/com/shake_demo/Person.java
@@ -0,0 +1,12 @@
+package test.liuyang.com.shake_demo;
+
+/**
+ * Created by Administrator on 2016/8/30.
+ */
+public class Person {
+
+    private  int id;
+    private  String name;
+    private  String  gender;
+
+}
